e75bf436eb920895db117ce08c176e25b15c048e
GDWR <gregory.dwr@gmail.com>
2022-07-24 15:43:11 +0100

    ✨ Add --version to yes

--- python/src/yes.py
+++ python/src/yes.py
@@ -3,7 +3,7 @@ from argparse import ArgumentParser
 
 parser = ArgumentParser(description="Repeatedly output a line with all specified STRING(s), or 'y'.")
 parser.add_argument('string', type=str, nargs="...")
-
+parser.add_argument('--version', action='version', version='%(prog)s 1.0')
 args = parser.parse_args()
 
 message = ' '.join(args.string) or "y"
